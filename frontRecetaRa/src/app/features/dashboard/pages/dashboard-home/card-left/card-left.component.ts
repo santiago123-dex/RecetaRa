ad3d8de3b44a37ff5d693e00c669b6933f9325ba
santiago123-dex <santiagofajardmorales17@gmail.com>
2025-11-11 09:33:42 -0500

    se sube organizazion de archivos del dashboard y tambien se inicia el diseño del dashboard

---
+++ frontRecetaRa/src/app/features/dashboard/pages/dashboard-home/card-left/card-left.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'card-left',
+    standalone: true,
+    imports: [],
+    templateUrl: './card-left.component.html',
+})
+
+export class CardLeft{
+    
+}
--- frontRecetaRa/src/app/features/dashboard/pages/dashboard-home/dashboard-home.component.ts
+++ frontRecetaRa/src/app/features/dashboard/pages/dashboard-home/dashboard-home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { CardMiddle } from './card-middle/card-middle.component';
+import { CardLeft } from './card-left/card-left.component';
 
 @Component({
   selector: 'app-dashboard-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CardMiddle, CardLeft],
   templateUrl: './dashboard-home.component.html',
   styleUrl: './dashboard-home.component.css'
 })
